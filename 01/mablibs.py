708bd8aac54dbdd2922c63ced441efd2fe4d1aff
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2024-04-21 21:15:58 +0100

    Pizza Madlibs Done.

---
+++ 01/mablibs.py
@@ -0,0 +1,25 @@
+print("*" * 50)
+
+print("This is fun Madlibs about Pizza. Fill in the words as per description to read the story of Pizza.")
+
+print("*" * 50)
+
+adj = input("Adjective: ")
+nationallity = input("Nationallity: ")
+person = input("Person's name: ")
+noun = input("Noun: ")
+adj2 = input("Adjective: ")
+noun2 = input("Noun: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+pl_noun = input("Plural Noun: ")
+noun3 = input("Noun: ")
+num = input("Number: ")
+shape = input("Shapes: ")
+food = input("Food: ")
+food2 = input("Food: ")
+num2 = input("Number: ")
+
+print(f"Pizza was invented by a {adj} {nationallity} chef named {person}. To make a pizza, you need to take a lump of {noun}, and make a thin, round {adj2} {noun2}. Then you cover it with {adj3} sauce, {adj4} cheese, and fresh chopped {pl_noun}. Next you have to bake it in a very hot {noun3}. When it is done, cut it into {num} {shape}. Some kids like {food} pizza the best, but my favorite is the {food2} pizza. If I could, I would eat pizza {num2} times a day!")
+
+
